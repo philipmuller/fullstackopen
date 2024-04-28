d2073dca3506321f7f17118a779942be5023872f
Philip Müller <philipmuller97@gmail.com>
2024-04-28 21:09:43 +0200

    2.4: Course information step9, 2.5: Separate module step10

--- Part2/courseinfo/src/App.jsx
+++ Part2/courseinfo/src/App.jsx
@@ -2,18 +2,35 @@ import Course from "./components/Course";
 
 const App = () => {
 
-  const course = {
-    id: 1,
-    name: 'Half Stack application development',
-    parts: [
-      {id: 1, name: 'Fundamentals of React', excercises: 10},
-      {id: 2, name: 'Using props to pass data', excercises: 7},
-      {id: 3, name: 'State of a component', excercises: 14},
-      {id: 4, name: 'Redux', excercises: 11},
-    ] 
-  }
+  const courses = [
+    {
+      id: 1,
+      name: 'Half Stack application development',
+      parts: [
+        {id: 1, name: 'Fundamentals of React', excercises: 10},
+        {id: 2, name: 'Using props to pass data', excercises: 7},
+        {id: 3, name: 'State of a component', excercises: 14},
+        {id: 4, name: 'Redux', excercises: 11},
+      ] 
+    }, 
+    {
+      id: 2,
+      name: 'Node.js',
+      parts: [
+        {id: 1, name: 'Routing', excercises: 3},
+        {id: 2, name: 'Middlewares', excercises: 7},
+      ]
+    }
+  ]
 
-  return (<Course course={course} />)
+  return (
+    <div>
+      <h1>Web Development Curriculum</h1>
+      {courses.map(course =>
+      <Course key={course.id} course={course}/>
+      )}
+    </div>
+  )
 }
 
 export default App
