68c43d1a05325bb956f5c43cde617a56e9fc5b72
Philip Müller <philipmuller97@gmail.com>
2024-04-29 00:35:43 +0200

    2.9*: The Phonebook Step 4

--- Part2/phonebook/src/App.jsx
+++ Part2/phonebook/src/App.jsx
@@ -4,7 +4,8 @@ import PersonInput from './components/PersonInput'
 const App = () => {
   const [persons, setPersons] = useState([
     { id: 1, name: 'Arto Hellas', number: '034-2928302' }
-  ]) 
+  ])
+  const [filter, setFilter] = useState('')
 
   const addPerson = ({name, number}) => {
     if (persons.some(person => person.name === name)) {
@@ -19,12 +20,24 @@ const App = () => {
     }
   }
 
+  const handleFilterChange = (event) => {
+    setFilter(event.target.value)
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+          filter shown with: <input value={filter} onChange={handleFilterChange} />
+      </div>
+      <h2>Add a new</h2>
       <PersonInput onSubmit={addPerson} />
       <h2>Numbers</h2>
-      {persons.map(person => <p key={person.id}>{person.name} {person.number}</p>)}
+      {persons
+      .filter(person => person.name.toLowerCase().includes(filter.toLowerCase()))
+      .map(person => 
+      <p key={person.id}>{person.name} {person.number}</p>
+      )}
     </div>
   )
 }
